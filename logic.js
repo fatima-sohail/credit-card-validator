146fc0c634e2d0aa2cf5c19bdb8624edc1bd354e
unknown <fatimasohailhunzai@gmail.com>
2023-05-30 19:52:09 -0400

    created skeleton for the credit card validator website

---
+++ logic.js
@@ -0,0 +1,65 @@
+let ccInput = '';
+document.addEventListener("DOMContentLoaded", function(){
+    var nextButtonRef = document.getElementById('nextState')
+    nextButtonRef.disabled = true;
+})
+
+function clearInputBox(){
+    let inputBoxRef = document.getElementById('inputId')
+    inputBoxRef.value = ''
+}
+
+
+
+
+function validateCC (){
+    let ccInputRef = document.getElementById('inputId')
+    ccInput = ccInputRef.value 
+    console.log(ccInput)
+}
+
+
+function validateCred(){
+    var ccValidateStatusRef = document.getElementById('ccValidateStatus')
+    var ccInputRef = document.getElementById('cardNumber')
+    let ccInput  = ccInputRef.value
+
+    if(ccInput == "") {
+        ccValidateStatusRef.innerText = "input cannot be empty"
+        return;
+    }
+
+    let arr = ccInput.split("")
+    let reverseArray = arr.reverse();
+
+    console.log(reverseArray)
+    
+
+    let sum = 0;
+    // console.log(reverseArray)
+    for(let i = 0; i<arr.length; i++){
+        let value = reverseArray[i];
+        value = Number(value)
+        if(i%2==1){
+            value = value*2
+            if(value > 9){
+                value = value - 9
+            }
+        }
+        sum = sum + value
+    }
+    
+    // let ccValidateStatus = ccValidateStatusRef.value
+    if(sum%10 == 0){
+        // get reference to status then update it
+        
+        ccValidateStatusRef.innerText = "Valid"
+
+    //    return 'valid' 
+
+    }else {
+        ccValidateStatusRef.innerText = "Invalid"
+    }
+}
+
+
